acb7b78f319f6abae104140ed161e1c67807af00
jaime Avendaño <jaime.aven.villa@gmail.com>
2022-02-11 15:01:56 -0500

    initial commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import 'react-native-gesture-handler';
+
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>hello world</Text>
+    </View>
+  )
+}
+
+export default App
