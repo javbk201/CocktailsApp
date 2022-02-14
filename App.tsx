7ec3dd09fde99faf3da63f6ba1ad2f1c72045275
gnaguerrer@gmail.com <86370821+gnaguerrer@users.noreply.github.com>
2022-02-14 09:38:55 -0500

    ⚡Feat: Drawer navigation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,22 @@
 import 'react-native-gesture-handler';
 
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text} from 'react-native';
+import React from 'react';
+import Home from '@/screens/Home';
+
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+
+const Drawer = createDrawerNavigator();
 
 const App = () => {
   return (
-    <View>
-      <Text>hello world</Text>
-    </View>
-  )
-}
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={Home} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+};
 
-export default App
+export default App;
